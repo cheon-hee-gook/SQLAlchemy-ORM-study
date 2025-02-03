2b6ea59378b52b168b469b3256214935c8ecd569
hg <chg9503@naver.com>
2025-02-03 22:20:15 +0900

    Add database setup and migrations

---
+++ app/models/flower.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from app.database import Base
+
+class Flower(Base):
+    __tablename__ = "flowers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    meaning = Column(String)
