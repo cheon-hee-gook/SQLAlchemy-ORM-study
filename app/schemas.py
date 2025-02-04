fe98538a6a134657fb97fc37875c565923d5940b
hg <chg9503@naver.com>
2025-02-04 23:40:21 +0900

    add flower schema with pydantic

---
+++ app/schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+
+class FlowerBase(BaseModel):
+    name: str
+    meaning: str
+
+
+class FlowerCreate(FlowerBase):
+    pass  # 추가할 때 필요한 필드 (name, meaning)
+
+
+class FlowerUpdate(FlowerBase):
+    pass  # 수정할 때 필요한 필드 (name, meaning)
+
+
+class FlowerResponse(FlowerBase):
+    id: int
+
+    class Config:
+        orm_mode = True  # SQLAlchemy 모델과 호환
