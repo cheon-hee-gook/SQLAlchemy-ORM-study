2b563e8d2f254a5e495094361551a2d15a6db07a
hg <chg9503@naver.com>
2025-02-05 00:27:23 +0900

    modify space area

--- app/database.py
+++ app/database.py
@@ -6,4 +6,5 @@ DATABASE_URL = "sqlite:///./flowers.db"  # SQLite 사용
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
